261bafd5916ad8ededbeddc404725e354669c33d
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2026-02-14 21:21:29 +0200

    Adding Light and Dark Mode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
 import '../src/index.css';
 
 const preview: Preview = {
@@ -10,6 +11,16 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-theme',
+    }),
+  ],
 };
 
 export default preview;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,5 +2,6 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.mdx'],
+  darkMode: ['class', '[data-theme="dark"]'],
   plugins: [],
 } satisfies Config;
